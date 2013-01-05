f91fc2036997e92a9d1a60e8ffeb5e7a7ce5c3eb
Italic- <super3dblender@yahoo.com>
2013-01-04 16:16:24 -0800

    added presets (render, themes)

---
+++ scripts/presets/render/AVC-I_100_1080i50.py
@@ -0,0 +1,11 @@
+import bpy
+bpy.context.scene.render.field_order = 'EVEN_FIRST'
+bpy.context.scene.render.fps = 25
+bpy.context.scene.render.fps_base = 1.0
+bpy.context.scene.render.pixel_aspect_x = 1.0
+bpy.context.scene.render.pixel_aspect_y = 1.0
+bpy.context.scene.render.resolution_percentage = 100
+bpy.context.scene.render.resolution_x = 1920
+bpy.context.scene.render.resolution_y = 1080
+bpy.context.scene.render.use_fields = True
+bpy.context.scene.render.use_fields_still = False
---
+++ scripts/presets/render/NTSC_DV_Widescreen_23.976.py
@@ -0,0 +1,11 @@
+import bpy
+bpy.context.scene.render.field_order = 'ODD_FIRST'
+bpy.context.scene.render.fps = 24
+bpy.context.scene.render.fps_base = 1.001
+bpy.context.scene.render.pixel_aspect_x = 40
+bpy.context.scene.render.pixel_aspect_y = 33
+bpy.context.scene.render.resolution_percentage = 100
+bpy.context.scene.render.resolution_x = 720
+bpy.context.scene.render.resolution_y = 480
+bpy.context.scene.render.use_fields = True
+bpy.context.scene.render.use_fields_still = False
